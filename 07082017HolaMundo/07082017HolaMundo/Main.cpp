5413c29264a394c86ccc5f127c3ad0074f24ba3b
drkalavera <808khz@gmail.com>
2017-08-08 09:20:41 -0500

    primer subida del semestre
    1era

---
+++ 07082017HolaMundo/07082017HolaMundo/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	/*
+	std::cout << "Hello, World!" << std::endl;
+	std::cin.get();
+	*/
+	cout << "Hello, World!" << endl;
+	cin.get();
+	return 0;
+}
